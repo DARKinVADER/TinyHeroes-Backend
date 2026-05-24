9e71d7af0cbf8cb122c7fc458e721ca23f8a163f
Gabor Czibik <gabor.czibik@sap.com>
2026-05-24 13:19:24 +0200

    mac stabilization

--- TinyHeroes.Tests/Integration/TestWebApplicationFactory.cs
+++ TinyHeroes.Tests/Integration/TestWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using TinyHeroes.Application.Interfaces;
@@ -15,6 +16,14 @@ public class TestWebApplicationFactory<TProgram> : WebApplicationFactory<TProgra
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["Storage:ConnectionString"] = $"disk://path={Path.Combine(Path.GetTempPath(), "tinyheroes-test-uploads")}"
+            });
+        });
 
         builder.ConfigureServices(services =>
         {

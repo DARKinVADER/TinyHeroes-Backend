ba2f3d8c67a6d38054c7473ebb58080686906b4b
Gabor Czibik <gabor.czibik@sap.com>
2026-05-31 07:07:00 +0200

    feat: add migration AddPrizeMinDeeds

---
+++ TinyHeroes.Infrastructure/Migrations/20260531050101_AddPrizeMinDeeds.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TinyHeroes.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrizeMinDeeds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MonthlyMinDeeds",
+                table: "Families",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WeeklyMinDeeds",
+                table: "Families",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonthlyMinDeeds",
+                table: "Families");
+
+            migrationBuilder.DropColumn(
+                name: "WeeklyMinDeeds",
+                table: "Families");
+        }
+    }
+}
